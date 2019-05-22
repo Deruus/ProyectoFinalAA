4412dfd99f8a662dbd603852fb81d51f8a7b514d
Deruus <dmantilla305@unab.edu.co>
2019-05-22 09:27:07 -0500

    commit 9:27 am

--- webapp.py
+++ webapp.py
@@ -52,25 +52,20 @@ def dream(fullpath, filename):
     img0 = np.float32(img0)
 
 
-    numero_aleatorio = random.randint(0,3)
-    if numero_aleatorio == 1:
-        layer_tensor = model.layer_tensors[2]
-        img0 = optimizacion_recursiva(layer_tensor=layer_tensor, image=img0,
-                                      num_iterations=10, step_size=3.0, rescale_factor=0.7,
-                                      num_repeats=4, blend=0.2)
-    elif numero_aleatorio == 2:
-        layer_tensor = model.layer_tensors[3]
-        img0 = optimizacion_recursiva(layer_tensor=layer_tensor, image=img0,
-                                      num_iterations=10, step_size=3.0, rescale_factor=0.7,
-                                      num_repeats=4, blend=0.2)
-    else:
-        layer_tensor = model.layer_tensors[7][:, :, :, 0:3]
-        optimizacion_recursiva(layer_tensor=layer_tensor, image=img0,
-                               num_iterations=10, step_size=3.0, rescale_factor=0.7,
-                               num_repeats=4, blend=0.2)
-    print(numero_aleatorio)
 
+    layer_tensor = model.layer_tensors[2]
+    img0 = optimizacion_recursiva(layer_tensor=layer_tensor, image=img0,
+                                      num_iterations=5, step_size=3.0, rescale_factor=0.7,
+                                      num_repeats=4, blend=0.2)
+    #layer_tensor = model.layer_tensors[3]
+    #img0 = optimizacion_recursiva(layer_tensor=layer_tensor, image=img0,
+    #                                  num_iterations=10, step_size=3.0, rescale_factor=0.7,
+    #                                  num_repeats=4, blend=0.2)
 
+    #layer_tensor = model.layer_tensors[7][:, :, :, 0:3]
+    #optimizacion_recursiva(layer_tensor=layer_tensor, image=img0,
+    #                           num_iterations=10, step_size=3.0, rescale_factor=0.7,
+    #                           num_repeats=4, blend=0.2)
 
 
 
