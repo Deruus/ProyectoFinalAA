19a7854a7bc7103d7aeca1fcfafde374edfa7e2a
Deruus <36492971+Deruus@users.noreply.github.com>
2019-05-22 16:18:16 -0500

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -53,7 +53,7 @@ def dream(fullpath, filename):
 
 
 
-    layer_tensor = model.layer_tensors[4]
+    layer_tensor = model.layer_tensors[2]
     #img0 = optimizacion_recursiva(layer_tensor=layer_tensor, image=img0,
     #                                  num_iterations=10, step_size=3.0, rescale_factor=0.7,
     #                                  num_repeats=4, blend=0.2)
